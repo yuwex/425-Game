1afc4f03a005fcc4a28863398f1f7a32f86cd26e
Noah <156490730+nkasica@users.noreply.github.com>
2024-09-21 19:28:47 -0400

    Revert "Merge branch 'main' of https://github.com/yuwex/425-Game"
    This reverts commit dac3a6ce8c86ad5aafe5f68ebe396aaa8cfa1d6e, reversing
    changes made to 18fb1219953564cc387eb1286013a2acaceca8be.

--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -17,7 +17,6 @@ public class TowerSpawner : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            print("here");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
