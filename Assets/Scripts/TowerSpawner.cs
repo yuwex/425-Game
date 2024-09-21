72233a0513fd90c913a1d26945f247a859caf4eb
Nightsvipe <rohan.mirza1296@gmail.com>
2024-09-21 13:27:43 -0400

    added spawning a sample tower wherever the player clicks on screen
    Tower Spawn Test

---
+++ Assets/Scripts/TowerSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class TowerSpawner : MonoBehaviour
+{
+    public GameObject TowerPlaceHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Instantiate(TowerPlaceHolder, hit.point, Quaternion.identity);
+            }
+            
+        }
+    }
+}
