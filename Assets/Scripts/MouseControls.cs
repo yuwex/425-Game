18fb1219953564cc387eb1286013a2acaceca8be
Noah <156490730+nkasica@users.noreply.github.com>
2024-09-21 19:19:22 -0400

    added first person controls

---
+++ Assets/Scripts/MouseControls.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControls : MonoBehaviour
+{
+
+    public float sens = 150f;
+    public Transform player;
+    private float xRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // lock cursor to center and make it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // retrieve mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sens;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sens;
+
+        xRotate -= mouseY;
+
+        // clamp player view rotation
+        xRotate = Mathf.Clamp(xRotate, -90, 90);
+
+        player.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotate, 0, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public CharacterController player;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float speed = 15.0f;
+    public float gravity = -40f;
+    
+    private Vector3 velocity;
+    private float groundDistance = 0.4f;
+    private float jumpHeight = 3;
+    private bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check to see if player is touching a grounded surface
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2;
+        }
+
+        // get wasd movements
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // calculate direction for player to move using local player coordinates
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // move player
+        player.Move(speed * Time.deltaTime * move);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded) 
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        // calculate gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        //apply gravity
+        player.Move(velocity * Time.deltaTime);
+    }
+}
