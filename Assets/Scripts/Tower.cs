092e062b1a746732dd046caa2516e8164e893e02
Carson <CarsonWDalziel@gmail.com>
2024-09-28 18:39:27 -0400

    added enemy targeting

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Transform target;
+    public float range = 20f;
+
+    public string enemyTag = "Enemy";
+
+    private UnityEngine.Vector3 enemyTarget = UnityEngine.Vector3.zero;
+
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
-    // Update is called once per frame
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = float.MaxValue;
+        GameObject closestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToTarget = UnityEngine.Vector3.Distance(enemy.transform.position, enemyTarget);
+            float distanceFromTower = UnityEngine.Vector3.Distance(enemy.transform.position, transform.position);
+
+            if (distanceFromTower < range && distanceToTarget < shortestDistance)
+            {
+                shortestDistance = distanceToTarget;
+                closestEnemy = enemy;
+            }
+        }
+
+        if (closestEnemy != null)
+        {
+            target = closestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
     void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
