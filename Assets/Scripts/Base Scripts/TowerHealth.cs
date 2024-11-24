df734f46c641e2577bf044ce2670860991d801d3
ifuller140 <ianfuller140@gmail.com>
2024-11-23 23:06:06 -0500

    Dump
    Just need to dump these changes here. They are not ready for main yet

--- Assets/Scripts/Base Scripts/TowerHealth.cs
+++ Assets/Scripts/Base Scripts/TowerHealth.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class TowerHealth : MonoBehaviour
 {
 
+    // public GameObject healthBarUI;
     public InfoBar bar;
     public int health = 100;
     public GameManager manager;
 
+    // private InfoBar uiBar;
+
 
     void Start()
     {
+<<<<<<< Updated upstream
         bar.setMaxValue(health);
         bar.setValue(health);
+=======
+        //uiBar = healthBarUI.GetComponentInChildren<InfoBar>();
+        bar.SetMaxValue(health);
+        bar.SetValue(health);
+        //uiBar.SetMaxValue(health);
+        //uiBar.SetValue(health);
+>>>>>>> Stashed changes
     }
 
     public void TowerDamage(int damage)
@@ -21,7 +32,13 @@ public class TowerHealth : MonoBehaviour
             Destroy(gameObject);
             manager.gameOver();
         }
+<<<<<<< Updated upstream
         health -= damage;
         bar.setValue(health);
+=======
+        health = 0;
+        bar.SetValue(health);
+        //uiBar.SetValue(health);
+>>>>>>> Stashed changes
     }
 }
