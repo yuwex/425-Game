5c829019744dc8c5ee664edda78ed3710aa25e2e
Carson <CarsonWDalziel@gmail.com>
2024-11-30 18:35:57 -0800

    no placing right on top of enemies

--- Assets/Scripts/Game Logic Scripts/BuildMode.cs
+++ Assets/Scripts/Game Logic Scripts/BuildMode.cs
@@ -113,6 +113,17 @@ public class TowerSpawner : MonoBehaviour
         }
 
         Vector3 pos = board.NormalizePos(hit.point);
+        
+        Collider[] nearby = Physics.OverlapSphere(pos, 2.5f);
+        foreach (Collider collider in nearby)
+        {
+            if (collider.gameObject.CompareTag("Enemy"))
+            {
+                canPlace = false;
+                break;
+            }
+        }
+
         var (towerX, towerY) = board.GetCoordinateFromPos(pos);
 
         GameObject tileObject = board.GetBoard(towerX, towerY);
